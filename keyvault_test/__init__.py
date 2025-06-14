e213320a8cfd5ba25d7c904d60bf894207be1851
Chris Wibberley <cwibberley@networkmotion.com>
2025-06-14 17:02:48 +0100

    Went back to basics

--- keyvault_test/__init__.py
+++ keyvault_test/__init__.py
@@ -2,9 +2,6 @@ import os
 import logging
 from typing import Optional
 import azure.functions as func
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
-from helper_functions.auth_helper import get_secret
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Key Vault test function triggered")
@@ -13,11 +10,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     key_vault_name = os.environ.get("KEY_VAULT_NAME")
    
     vault_url = f"https://{key_vault_name}.vault.azure.net"
-    credential = DefaultAzureCredential()
-    client = SecretClient(vault_url=vault_url, credential=credential)
+    #credential = DefaultAzureCredential()
+    #client = SecretClient(vault_url=vault_url, credential=credential)
     
-    secret = client.get_secret(secret_name)
-    return func.HttpResponse(secret.value if secret.value else "Bad", status_code=200)
+    #secret = client.get_secret(secret_name)
+    #return func.HttpResponse(secret.value if secret.value else "Bad", status_code=200)
+    return func.HttpResponse("finally")
 
 
 
