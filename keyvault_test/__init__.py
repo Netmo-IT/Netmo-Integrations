076f7e230a37e6dbb242a7d4cd04cc6ad5fa66ac
Chris Wibberley <cwibberley@networkmotion.com>
2025-06-14 21:05:58 +0100

    Enabled key vault

--- keyvault_test/__init__.py
+++ keyvault_test/__init__.py
@@ -3,21 +3,21 @@ import logging
 from typing import Optional
 import azure.functions as func
 from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Key Vault test function triggered")
 
-    secret_name = "API-Pax8-Client-ID"
+    secret_name = "API-Pax8-Client-ID" # rltxn3Sl7WCMPxzFnOLkzgIaXM8N2K9v
     key_vault_name = os.environ.get("KEY_VAULT_NAME")
    
     vault_url = f"https://{key_vault_name}.vault.azure.net"
-    #credential = DefaultAzureCredential()
-    #client = SecretClient(vault_url=vault_url, credential=credential)
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=vault_url, credential=credential)
     
-    #secret = client.get_secret(secret_name)
-    #return func.HttpResponse(secret.value if secret.value else "Bad", status_code=200)
-    return func.HttpResponse("finally")
+    secret = client.get_secret(secret_name)
+    return func.HttpResponse(secret.value if secret.value else "Error", status_code=200)
 
 
 
